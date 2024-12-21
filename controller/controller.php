f75b2eb222461c175e5bc568c7706d23f6e1710f
axelle <kadance9720@gmail.com>
2024-12-21 16:20:12 +0100

    Fix signup redirection

--- controller/controller.php
+++ controller/controller.php
@@ -1,101 +1,104 @@
 <?php
 
-    // require_Once assure qu'on ne charge qu'une fois les classes
-    require_once('model/AccountManager.php');
-    require_once('model/CommentManager.php');
-    require_once('model/PostManager.php');
+// require_Once assure qu'on ne charge qu'une fois les classes
+require_once('model/AccountManager.php');
+require_once('model/CommentManager.php');
+require_once('model/PostManager.php');
 
-    //connexion/inscription
-    function sign_Up() {
+//connexion/inscription
+function sign_Up()
+{
 
-        $signUpManager = new \MonEntreprise\Bloggi\Model\AccountManager();
-        $sigUp = $signUpManager->signUp();
+    $signUpManager = new \MonEntreprise\Bloggi\Model\AccountManager();
+    $sigUp = $signUpManager->signUp();
 
-        header('Location: view/connexion.php');
+    header('Location: index.php');
+}
 
-        //require('view/inscription.php');
+function log_In()
+{
+
+    $logInManager = new \MonEntreprise\Bloggi\Model\AccountManager();
+    $logIn = $logInManager->logIn();
+
+    header('Location: index.php?action=allPosts');
+}
+
+//Routeur page
+
+function allposts()
+{
+
+    $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
+    $posts = $postManager->getPosts();
+
+    require('view/indexView.php');
+}
+
+function post()
+{
+
+    $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
+    $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
+
+    if (isset($_GET['id']) && $_GET['id'] > 0) {
+        $post = $postManager->getPost($_GET['id']);
+        $comments = $commentManager->getComments($_GET['id']);
+    } else {
+        echo 'Erreur : aucun identifiant de billet envoyé';
     }
 
-    function log_In() {
+    require('view/postView.php');
+}
 
-        $logInManager = new \MonEntreprise\Bloggi\Model\AccountManager();
-        $logIn = $logInManager->logIn();
+function addPost()
+{
 
+    $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
+    $reqAddPost = $postManager->post_Form();
+
+    if ($reqAddPost === false) {
+        // Erreur gérée. Elle sera remontée jusqu'au bloc try du routeur !
+        throw new Exception('Impossible d\' ajouter le post!');
+    } else {
+        require('view/post_form.php');
+    }
+
+    //require('view/post_form.php');
+
+}
+
+function addComment()
+{
+
+    $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
+    $reqAddComment = $commentManager->postComment();
+
+    if ($reqAddComment === false) {
+        throw new Exception('Impossible d\' ajouter le commentaire!');
+    } else {
+        header('Location: index.php?action=post&id=' . $_GET['id'] . '');
+    }
+}
+
+function UpdateComment()
+{
+
+    $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
+    $reqUpdateComment = $commentManager->Update_Comment();
+
+    if ($reqUpdateComment === false) {
+        throw new Exception('Impossible de modifier le commentaire!');
+    } else {
         header('Location: index.php?action=allPosts');
     }
+}
 
-    //Routeur page
+function m_Account()
+{
 
-    function allposts() {
+    $accountManager = new \MonEntreprise\Bloggi\Model\AccountManager();
+    $getMember = $accountManager->memberAccount();
 
-        $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
-        $posts = $postManager->getPosts();
-
-        require('view/indexView.php');
-    }
-
-    function post() {
-
-        $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
-        $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
-
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            $post = $postManager->getPost($_GET['id']);
-            $comments = $commentManager->getComments($_GET['id']);
-        } else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-        }
-    
-        require('view/postView.php');
-    }
-
-    function addPost() {
-
-        $postManager = new \MonEntreprise\Bloggi\Model\PostManager();
-        $reqAddPost = $postManager->post_Form();
-
-        if ($reqAddPost === false) {
-            // Erreur gérée. Elle sera remontée jusqu'au bloc try du routeur !
-            throw new Exception('Impossible d\' ajouter le post!');
-        } else {
-            require('view/post_form.php');
-        }
-        
-        //require('view/post_form.php');
-
-    }
-
-    function addComment() {
-
-        $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
-        $reqAddComment = $commentManager->postComment();
-
-        if ($reqAddComment === false) {
-            throw new Exception('Impossible d\' ajouter le commentaire!');
-        } else {
-            header('Location: index.php?action=post&id=' . $_GET['id'] . '');
-        }
-        
-    }
-
-    function UpdateComment() {
-
-        $commentManager = new \MonEntreprise\Bloggi\Model\CommentManager();
-        $reqUpdateComment = $commentManager->Update_Comment();
-
-        if ($reqUpdateComment === false) {
-            throw new Exception('Impossible de modifier le commentaire!');
-        } else {
-            header('Location: index.php?action=allPosts');
-        }
-        
-    }
-
-    function m_Account() {
-
-        $accountManager = new \MonEntreprise\Bloggi\Model\AccountManager();
-        $getMember = $accountManager->memberAccount();
-
-        require('view/memberAccount.php');
-
-    }
+    require('view/memberAccount.php');
+}
--- index.php
+++ index.php
@@ -1,87 +1,71 @@
 <?php
-    require('controller/controller.php');
+require('controller/controller.php');
 
-    try {
-        if (isset($_GET['action'])) {
-            if ($_GET['action'] == 'allPosts') {
-                allPosts();
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'allPosts') {
+            allPosts();
+        } elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) { // on test la présence de l'Id dans l'url
+                post();
+            } else { // On arrête tout, on envoie une exception, donc on saute directement au catch
+                throw new Exception('Erreur : aucun identifiant de billet envoyé');
             }
-            elseif ($_GET['action'] == 'post') {
-                if (isset($_GET['id']) && $_GET['id'] > 0) { // on test la présence de l'Id dans l'url
-                    post();
-                }
-                else { // On arrête tout, on envoie une exception, donc on saute directement au catch
-                    throw new Exception('Erreur : aucun identifiant de billet envoyé');
-                }
+        } elseif ($_GET['action'] == 'addPost') {
+            addPost();
+
+            if (!empty($_POST['auteur']) && !empty($_POST['auteur']) && !empty($_POST['auteur'])) {
+                addPost($_POST['auteur'], $_POST['auteur'], $_POST['auteur']);
+            } else {
+                throw new Exception('Erreur : tous les champs ne sont pas remplis !');
             }
-            elseif ($_GET['action'] == 'addPost') {
-                addPost();
-                
-                if (!empty($_POST['auteur']) && !empty($_POST['auteur']) && !empty($_POST['auteur'])) {
-                    addPost($_POST['auteur'], $_POST['auteur'], $_POST['auteur']);
+
+            //require('view/post_form.php');
+        } elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['auteur']) && !empty($_POST['commentaire'])) {
+                    addComment($_GET['id'], $_POST['auteur'], $_POST['commentaire']);
                 } else {
                     throw new Exception('Erreur : tous les champs ne sont pas remplis !');
                 }
-                
-                //require('view/post_form.php');
+            } else {
+                throw new Exception('Erreur : aucun identifiant de billet envoyé');
             }
-            elseif ($_GET['action'] == 'addComment') {
-                if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    if (!empty($_POST['auteur']) && !empty($_POST['commentaire'])) {
-                        addComment($_GET['id'], $_POST['auteur'], $_POST['commentaire']);
-                    } 
-                    else {
-                        throw new Exception('Erreur : tous les champs ne sont pas remplis !');
-                    }
+        } elseif ($_GET['action'] == 'updateComment') {
+            if ((isset($_GET['id']) && $_GET['id'] > 0)) {
+                if (!empty($_POST['commentaire'])) {
+                    updateComment($_GET['id'], $_GET['auteur'], $_POST['commentaire']);
+                } else {
+                    throw new Exception('Erreur : tous les champs ne sont pas remplis !');
                 }
-                else {
-                    throw new Exception('Erreur : aucun identifiant de billet envoyé');
-                }
+            } else {
+                throw new Exception('Erreur : aucun identifiant de billet envoyé');
             }
-            elseif ($_GET['action'] == 'updateComment') {
-                if ((isset($_GET['id']) && $_GET['id'] > 0)) {
-                    if (!empty($_POST['commentaire'])) {
-                        updateComment($_GET['id'], $_GET['auteur'], $_POST['commentaire']);
-                    } 
-                    else {
-                        throw new Exception('Erreur : tous les champs ne sont pas remplis !');
-                    }
-                }
-                else {
-                    throw new Exception('Erreur : aucun identifiant de billet envoyé');
-                }    
+        } elseif ($_GET['action'] == 'signUp_page') {
+            require('view/inscription.php');
+        } elseif ($_GET['action'] == 'sign_Up') {
+            if (!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['mot_de_passe']) && !empty($_POST['mot_de_passe_verification'])) {
+                sign_Up($_POST['pseudo'], $_POST['email'], $_POST['mot_de_passe'], $_POST['mot_de_passe_verification']);
+            } else {
+                throw new Exception('Erreur : Inscription impossible, tous les champs ne sont pas remplis !');
             }
-            elseif ($_GET['action'] == 'sign_Up') {
-                if (!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['mot_de_passe']) && !empty($_POST['mot_de_passe_verification'])) {
-                    sign_Up($_POST['pseudo'], $_POST['email'], $_POST['mot_de_passe'], $_POST['mot_de_passe_verification']);
-                } 
-                else {
-                    throw new Exception('Erreur : Inscription impossible, tous les champs ne sont pas remplis !');
-                }
+        } elseif ($_GET['action'] == 'log_In') {
+            if (!empty($_POST['email']) && !empty($_POST['mot_de_passe'])) {
+                log_In($_POST['email'], $_POST['mot_de_passe']);
+            } else {
+                throw new Exception('Erreur : Connexion impossible, tous les champs ne sont pas remplis !');
             }
-            elseif ($_GET['action'] == 'log_In') {
-                if (!empty($_POST['email']) && !empty($_POST['mot_de_passe'])) {
-                    log_In($_POST['email'], $_POST['mot_de_passe']);
-                } 
-                else {
-                    throw new Exception('Erreur : Connexion impossible, tous les champs ne sont pas remplis !');
-                }
-            }
-            elseif ($_GET['action'] == 'memberAccount') {
-                m_Account();
-            }
+        } elseif ($_GET['action'] == 'memberAccount') {
+            m_Account();
         }
-        else {
+    } else {
+        require('view/connexion.php');
+    }
+} catch (Exception $e) { // S'il y a eu une erreur, alors ...
 
-            require('view/connexion.php');
-            //require('view/inscription.php');
-        }
-        
-    } catch (Exception $e) { // S'il y a eu une erreur, alors ...
+    echo 'Erreur : ' . $e->getMessage();
 
-        echo 'Erreur : ' . $e->getMessage();
-
-        /*$errorMessage = $e->getMessage();
+    /*$errorMessage = $e->getMessage();
 
         require('view/errorView.php');*/
-    }
+}
